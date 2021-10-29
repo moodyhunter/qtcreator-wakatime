473f5c1e243b63fe17c1fe7ca77820ae516134d2
brian.orwe <brian.orwe@gmail.com>
2021-10-29 17:00:19 +0300

    Try fix

--- waka_plugin.cpp
+++ waka_plugin.cpp
@@ -47,13 +47,12 @@ void WakaPlugin::showMessagePrompt(const QString str){
     if(_wakaOptions.isNull())
         return;
 
-    QTC_ASSERT(!_wakaOptions->isDebug(),
+    if(_wakaOptions->isDebug())
 #if IDE_LESS_15_VERSION>0
-   Core::MessageManager::write(str)
+       Core::MessageManager::write(str);
 #else
-   Core::MessageManager::writeDisrupting(QString(str))
+       Core::MessageManager::writeDisrupting(QString(str));
 #endif
-    );
 }
 
 QDir WakaPlugin::getWakaCLILocation(){
